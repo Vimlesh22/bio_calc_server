883ef319b0e5fba21663499693263647737fea87
Vimlesh Kumar <kumarvimlesh007@gmail.com>
2023-09-23 13:30:45 -0500

    testing api by calculating trips and depreciation

--- controller/bm_controller.js
+++ controller/bm_controller.js
@@ -14,13 +14,23 @@
 /**
 *@description loads all the depencies requied for the user controller class including express validator
 */
-const { validationResult } = require('express-validator');
 
 const Response = require('../services/Response');
 const Service = require('../services/bm_service');
 
 module.exports.calculateBm = async (req, res) => {
     try {
+
+        const inputData = req.body;
+        const results = Service.calculateBmService(inputData);
+
+        if (results && results.length > 0) {
+            let response = new Response(false, "Calculation done successfully", results);
+            res.status(200).send(response);
+        } else {
+            let response = new Response(true, "Error while calculation", []);
+            res.status(400).send(response);
+        }
     } catch (error) {
         // Handle any unexpected errors here
         console.error(error);
--- services/bm_service.js
+++ services/bm_service.js
@@ -16,6 +16,29 @@
 
 const calculateBM = (inputData) => {
     try {
+        const {
+            yield,
+            truck_capacity,
+            purchase,
+            sales
+        } = inputData;
+
+        // Calculate trip_per_acre using the formula (yield / truck_capacity)
+        const trip_per_acre = yield / truck_capacity;
+
+        // Calculate depreciation using the formula (purchase - sales)
+        const depreciation = purchase - sales;
+
+        // Create an array of objects to hold the calculated values
+        const calculatedValues = [
+            {
+                "trip_per_acre": trip_per_acre,
+                "depreciation": depreciation
+            }
+        ];
+
+        // Return the array of calculated values
+        return calculatedValues;
     } catch (error) {
         console.error("Error calculating trip_per_acre and depreciation:", error);
         throw error;
